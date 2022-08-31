6fe899548e83ec5074871d4814bb26d00a375d86
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-01 01:07:15 +0300

    add step deploy in git actions v.78

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -4,7 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/', include('api.urls')),
-    path('api/', include('users.urls', namespace='users')),
     path('admin/', admin.site.urls),
     path(
         'redoc/',
