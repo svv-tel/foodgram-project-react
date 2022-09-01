04e26d8d7b65a890ac69eced0f6504bbfa507368
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-01 22:43:12 +0300

    add step deploy in git actions v.83 add load ingredients

--- backend/foodgram/recipes/management/commands/load_ingredients.py
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -1,22 +1,27 @@
 import json
 
-from django.conf import settings
-from django.core.management import BaseCommand
+from django.core.management.base import BaseCommand
 
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Загрузка данных в базу из json-файла'
+    help = 'Fill database with ingredients'
 
     def handle(self, *args, **kwargs):
-        data_path = settings.BASE_DIR
-        with open(
-                f'{data_path}/data/ingredients.json',
-                'r',
-                encoding='utf-8'
-        ) as file:
-            data = json.load(file)
-            Ingredient.objects.bulk_create(
-                Ingredient(**one_ingredient) for one_ingredient in data)
-        self.stdout.write(self.style.SUCCESS('Ингредиенты были загружены!'))
+        file = open('recipes/management/commands/data/ingredients.json',
+                    encoding='utf-8')
+        data = json.loads(file.read())
+        ingredient_objs = []
+        for ingredient in data:
+            name = ingredient['name']
+            measurement_unit = ingredient['measurement_unit']
+            ingredient_objs.append(
+                Ingredient(
+                    name=name,
+                    measurement_unit=measurement_unit,
+                )
+                )
+        Ingredient.objects.bulk_create(
+            ingredient_objs, ignore_conflicts=True
+        )
