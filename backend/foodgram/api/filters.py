dc6f1fd301302bf93865620eb44fc0124df44d0e
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-03 00:20:58 +0300

    add step deploy in git actions v.103 filter tag

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,29 +1,16 @@
 from django_filters.rest_framework import BaseInFilter, CharFilter, FilterSet
-
+from django.contrib.auth import get_user_model
 from recipes.models import Ingredient, Recipe
+
+from django_filters import rest_framework as filters
+from recipes.models import Tag
+
+User = get_user_model()
 
 
 class CharFilterInFilter(BaseInFilter, CharFilter):
     pass
 
-
-class RecipeFilter(FilterSet):
-    tags = CharFilterInFilter(
-        field_name='tags__slug',
-        lookup_expr='in'
-    )
-    name = CharFilter(
-        field_name='name',
-        lookup_expr='contains'
-    )
-    author = CharFilterInFilter(
-        field_name='author__pk',
-        lookup_expr='in'
-    )
-
-    class Meta:
-        model = Recipe
-        fields = ('tags', 'name')
 
 
 class IngredientFilter(FilterSet):
@@ -35,3 +22,34 @@ class IngredientFilter(FilterSet):
     class Meta:
         model = Ingredient
         fields = ('name',)
+
+
+class RecipeFilter(filters.FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        queryset=Tag.objects.all(),
+        to_field_name='slug',
+    )
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    is_favorited = filters.NumberFilter(method='get_is_favorited')
+    is_in_shopping_cart = filters.NumberFilter(
+        method='get_is_in_shopping_cart'
+    )
+
+    class Meta:
+        model = models.Recipe
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+
+    def get_is_favorited(self, queryset, name, value):
+        if value:
+            return models.Recipe.objects.filter(
+                favorite_recipe__user=self.request.user
+            )
+        return models.Recipe.objects.all()
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if value:
+            return models.Recipe.objects.filter(
+                shopping_cart__user=self.request.user
+            )
+        return models.Recipe.objects.all()
