b6797f1af7cf7b0ee8bf1e73a998e88410205707
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-04 01:45:38 +0300

    add step deploy in git actions v.121

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
