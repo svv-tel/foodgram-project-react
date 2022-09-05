0d2aec9bf383e88d66de9016a7f2ca217a999d3b
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-05 23:40:16 +0300

    add step deploy in git actions v.128

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,48 +1,38 @@
-from django.contrib.auth import get_user_model
-from django_filters import rest_framework as filters
-from recipes import models
-from recipes.models import Tag
+from django_filters.rest_framework import BaseInFilter, CharFilter, FilterSet
 
-User = get_user_model()
+from recipes.models import Ingredient, Recipe
 
 
-class RecipeFilter(filters.FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
+class CharFilterInFilter(BaseInFilter, CharFilter):
+    pass
+
+
+class RecipeFilter(FilterSet):
+    tags = CharFilterInFilter(
         field_name='tags__slug',
-        queryset=Tag.objects.all(),
-        to_field_name='slug',
+        lookup_expr='in'
     )
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    is_favorited = filters.NumberFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.NumberFilter(
-        method='get_is_in_shopping_cart'
-    )
-
-    class Meta:
-        model = models.Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
-
-    def get_is_favorited(self, queryset, name, value):
-        if value:
-            return models.Recipe.objects.filter(
-                favorite_recipe__user=self.request.user
-            )
-        return models.Recipe.objects.all()
-
-    def get_is_in_shopping_cart(self, queryset, name, value):
-        if value:
-            return models.Recipe.objects.filter(
-                shopping_cart__user=self.request.user
-            )
-        return models.Recipe.objects.all()
-
-
-class IngredientFilter(filters.FilterSet):
-    name = filters.CharFilter(
+    name = CharFilter(
         field_name='name',
-        lookup_expr='istartswith'
+        lookup_expr='contains'
+    )
+    author = CharFilterInFilter(
+        field_name='author__pk',
+        lookup_expr='in'
     )
 
     class Meta:
-        model = models.Ingredient
+        model = Recipe
+        fields = ('tags', 'name')
+
+
+class IngredientFilter(FilterSet):
+    name = CharFilter(
+        field_name='name',
+        lookup_expr='contains'
+    )
+
+    class Meta:
+        model = Ingredient
         fields = ('name',)
+        
