776d6343819052fc021d03ef6c2cfb64b7a399cc
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-06 01:12:24 +0300

    add step deploy in git actions v.137

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,46 +1,31 @@
-from django.contrib.auth import get_user_model
 from django_filters import rest_framework as filters
-from recipes import models
-from recipes.models import Tag
 
-User = get_user_model()
+from recipes.models import Ingredient, Recipe
 
 
 class RecipeFilter(filters.FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        queryset=Tag.objects.all(),
-        to_field_name='slug',
-    )
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    is_favorited = filters.NumberFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.NumberFilter(
-        method='get_is_in_shopping_cart'
-    )
+    tags = filters.AllValuesMultipleFilter(field_name='receipttag__tag__slug')
+    is_favorited = filters.BooleanFilter(method='get_favorite')
+    is_in_shopping_cart = filters.BooleanFilter(method='get_in_shopping_cart')
 
     class Meta:
-        model = models.Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+        model = Recipe
+        fields = ('is_favorited', 'is_in_shopping_cart', 'author', 'tags')
 
-    def get_is_favorited(self, queryset, name, value):
-        user = self.request.user
-        if value == 1:
-            return queryset.filter(favorite_recipe__user=user)
-        return queryset
+    def get_favorite(self, queryset, name, value):
+        if value:
+            return Recipe.objects.filter(favorites__user=self.request.user)
+        return Recipe.objects.all()
 
-    def get_is_in_shopping_cart(self, queryset, name, value):
-        user = self.request.user
-        if value == 1:
-            return queryset.filter(cart_recipe__user=user)
-        return queryset
+    def get_in_shopping_cart(self, queryset, name, value):
+        if value:
+            return Recipe.objects.filter(shopping_cart__user=self.request.user)
+        return Recipe.objects.all()
 
 
 class IngredientFilter(filters.FilterSet):
-    name = filters.CharFilter(
-        field_name='name',
-        lookup_expr='istartswith'
-    )
+    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
 
     class Meta:
-        model = models.Ingredient
+        model = Ingredient
         fields = ('name',)
