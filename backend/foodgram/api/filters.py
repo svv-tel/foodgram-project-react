1a5aa21e71a3bd35fbeab28de4015e6966461d52
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-06 01:17:13 +0300

    add step deploy in git actions v.138

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,30 +1,36 @@
-from django_filters import rest_framework as filters
+from django_filters.rest_framework import BaseInFilter, CharFilter, FilterSet
 
 from recipes.models import Ingredient, Recipe
 
 
-class RecipeFilter(filters.FilterSet):
-    tags = filters.AllValuesMultipleFilter(field_name='receipttag__tag__slug')
-    is_favorited = filters.BooleanFilter(method='get_favorite')
-    is_in_shopping_cart = filters.BooleanFilter(method='get_in_shopping_cart')
+class CharFilterInFilter(BaseInFilter, CharFilter):
+    pass
+
+
+class RecipeFilter(FilterSet):
+    tags = CharFilterInFilter(
+        field_name='tags__slug',
+        lookup_expr='in'
+    )
+    name = CharFilter(
+        field_name='name',
+        lookup_expr='contains'
+    )
+    author = CharFilterInFilter(
+        field_name='author__pk',
+        lookup_expr='in'
+    )
 
     class Meta:
         model = Recipe
-        fields = ('is_favorited', 'is_in_shopping_cart', 'author', 'tags')
-
-    def get_favorite(self, queryset, name, value):
-        if value:
-            return Recipe.objects.filter(favorites__user=self.request.user)
-        return Recipe.objects.all()
-
-    def get_in_shopping_cart(self, queryset, name, value):
-        if value:
-            return Recipe.objects.filter(shopping_cart__user=self.request.user)
-        return Recipe.objects.all()
+        fields = ('tags', 'name')
 
 
-class IngredientFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+class IngredientFilter(FilterSet):
+    name = CharFilter(
+        field_name='name',
+        lookup_expr='contains'
+    )
 
     class Meta:
         model = Ingredient
