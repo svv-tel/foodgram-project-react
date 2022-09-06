0be6528d23c8656efb990d08094e60607c7f8b2b
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-06 12:49:19 +0300

    add step deploy in git actions v.155

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     'django_filters',
     'api.apps.ApiConfig',
     'users.apps.UsersConfig',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -41,7 +40,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'foodgram.urls'
@@ -149,7 +147,3 @@ DJOSER = {
         "user": ["djoser.permissions.CurrentUserOrAdminOrReadOnly"],
     }
 }
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from django.conf import settings
 
 urlpatterns = [
     path('api/', include('api.urls')),
@@ -12,8 +11,3 @@ urlpatterns = [
         name='redoc'
     ),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
