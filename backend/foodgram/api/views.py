d2be2c7fec43e2ee341ee3c24ac128ce9804e31a
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-13 01:54:22 +0300

    add step deploy in git actions v.235

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,10 +1,8 @@
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, status, viewsets
-from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework.pagination import PageNumberPagination
