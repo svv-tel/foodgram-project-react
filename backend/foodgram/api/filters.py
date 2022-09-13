b772151ab8d65853a5c734a96df53f72a09bfada
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-13 23:05:12 +0300

    add step deploy in git actions v.239 изменил filters

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,37 +1,39 @@
-from django_filters.rest_framework import BaseInFilter, CharFilter, FilterSet
+import django_filters as filters
 
 from recipes.models import Ingredient, Recipe
 
 
-class CharFilterInFilter(BaseInFilter, CharFilter):
+class CharInFilter(filters.BaseInFilter, filters.CharFilter):
     pass
 
 
-class RecipeFilter(FilterSet):
-    tags = CharFilterInFilter(
-        field_name='tags__slug',
-        lookup_expr='in'
-    )
-    name = CharFilter(
-        field_name='name',
-        lookup_expr='contains'
-    )
-    author = CharFilterInFilter(
-        field_name='author__pk',
-        lookup_expr='in'
+class RecipeFilter(filters.FilterSet):
+    tags = filters.AllValuesMultipleFilter(field_name="tags__slug")
+    is_favorited = filters.NumberFilter(method="get_is_favorited")
+    is_in_shopping_cart = filters.NumberFilter(
+        method="get_is_in_shopping_cart"
     )
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'name')
+        fields = ("tags", "author", "is_favorited", "is_in_shopping_cart")
+
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value == 1:
+            return queryset.filter(favorite_recipe__user=user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value == 1:
+            return queryset.filter(shopping_cart__user=user)
+        return queryset
 
 
-class IngredientFilter(FilterSet):
-    name = CharFilter(
-        field_name='name',
-        lookup_expr='contains'
-    )
+class IngredientFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name="name", lookup_expr="istartswith")
 
     class Meta:
         model = Ingredient
-        fields = ('name',)
+        fields = ("name",)
