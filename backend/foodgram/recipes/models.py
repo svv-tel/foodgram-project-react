3d2b4cdc4e4efa3251c6b982351cd55a2e848c7a
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-16 00:44:20 +0300

    review v.2.3.14

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models.constraints import UniqueConstraint
-from colorfield.fields import ColorField
 
 User = get_user_model()
 
