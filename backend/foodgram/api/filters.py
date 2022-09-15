d169b876a1284424007d0bf13d4e6cf91965f05e
Владимир Самошкин <svv-tel@yandex.ru>
2022-09-15 23:00:53 +0300

    review v.2.3.10.1

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,25 +1,39 @@
-from django_filters import rest_framework as filters
+import django_filters as filters
 
-from .models import Recipe, Tag
+from recipes.models import Ingredient, Recipe
+
+
+class CharInFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
 
 
 class RecipeFilter(filters.FilterSet):
-    author = filters.NumberFilter(
-        field_name='author__id',
-        lookup_expr='exact'
-    )
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all()
-    )
-    is_favorited = filters.BooleanFilter(
-        field_name='is_favorited'
-    )
-    is_in_shopping_cart = filters.BooleanFilter(
-        field_name='is_in_shopping_cart'
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    is_favorited = filters.NumberFilter(method='get_is_favorited')
+    is_in_shopping_cart = filters.NumberFilter(
+        method='get_is_in_shopping_cart'
     )
 
     class Meta:
         model = Recipe
-        fields = ['author', 'tags', 'is_favorited', 'is_in_shopping_cart']
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value == 1:
+            return queryset.filter(favorite_recipe__user=user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value == 1:
+            return queryset.filter(shopping_cart__user=user)
+        return queryset
+
+
+class IngredientFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name='name', lookup_expr='istartswith')
+
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
